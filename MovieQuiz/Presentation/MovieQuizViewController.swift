687ad5036c2478727efcae55275bc1b2a2f6d270
EvgeniyKuzin <vaglover@yandex.ru>
2024-05-04 01:03:06 +0300

    выполнен весь функционал согласно ТЗ

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -4,20 +4,28 @@ final class MovieQuizViewController: UIViewController {
     
     private var currentQuestionIndex = 0
     private var correctAnswers = 0
+    private let bigFont = UIFont(name: "YSDisplay-Medium", size: 23)
+    private let mediumFont = UIFont(name: "YSDisplay-Medium", size: 20)
     
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var counterLabel: UILabel!
+    @IBOutlet private var noButton: UIButton!
+    @IBOutlet private var yesButton: UIButton!
+    @IBOutlet var staticTopLabel: UILabel!
     
-    
-    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textLabel.font = bigFont
+        counterLabel.font = mediumFont
+        noButton.titleLabel?.font = mediumFont
+        yesButton.titleLabel?.font = mediumFont
+        staticTopLabel.font = mediumFont
         show(quiz: convert(model: questions[currentQuestionIndex]))
     }
     
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
-      // Попробуйте написать код конвертации самостоятельно
         let questionStep = QuizStepViewModel(
             image: UIImage(named: model.image) ?? UIImage(),
             question: model.text,
@@ -32,12 +40,51 @@ final class MovieQuizViewController: UIViewController {
     }
     
     private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect {
+            correctAnswers += 1
+        }
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
         imageView.layer.cornerRadius = 20
         imageView.layer.borderColor = isCorrect
         ? UIColor.ypGreen.cgColor
         : UIColor.ypRed.cgColor
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQuestionOrResults()
+        }
+    }
+    
+    private func showNextQuestionOrResults() {
+        if currentQuestionIndex == questions.count - 1 {
+            let text = "Ваш результат: \(correctAnswers)/10"
+            let viewModel = QuizResultsViewModel(
+                title: "Этот раунд закончен",
+                text: text,
+                buttonText: "Сыграть еще раз")
+            show(quiz: viewModel)
+            correctAnswers = 0
+        } else {
+            currentQuestionIndex += 1
+            show(quiz: convert(model: questions[currentQuestionIndex]))
+        }
+    }
+    
+    private func show(quiz result: QuizResultsViewModel){
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert
+        )
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+            self.currentQuestionIndex = 0
+            self.show(quiz: self.convert(model: questions[self.currentQuestionIndex]))
+            self.imageView.layer.borderColor = UIColor.ypBackground.cgColor
+        }
+        
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
     }
     
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
@@ -46,7 +93,6 @@ final class MovieQuizViewController: UIViewController {
     
     @IBAction private func noButttonClicked(_ sender: UIButton) {
         showAnswerResult(isCorrect: questions[currentQuestionIndex].correctAnswer == false)
-
     }
 }
 
@@ -57,10 +103,17 @@ struct QuizQuestion {
 }
 
 struct QuizStepViewModel {
-  let image: UIImage
-  let question: String
-  let questionNumber: String
+    let image: UIImage
+    let question: String
+    let questionNumber: String
 }
+
+struct QuizResultsViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
+
 
 private let questions: [QuizQuestion] = [
     QuizQuestion(
@@ -104,73 +157,3 @@ private let questions: [QuizQuestion] = [
         text: "Рейтинг этого фильма больше чем 6?",
         correctAnswer: false)
 ]
-
-
-
-
-
-
-
-/*
- Mock-данные
- 
- 
- Картинка: The Godfather
- Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Dark Knight
- Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Kill Bill
- Настоящий рейтинг: 8,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Avengers
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Deadpool
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Green Knight
- Настоящий рейтинг: 6,6
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Old
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: The Ice Age Adventures of Buck Wild
- Настоящий рейтинг: 4,3
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Tesla
- Настоящий рейтинг: 5,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Vivarium
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- */
