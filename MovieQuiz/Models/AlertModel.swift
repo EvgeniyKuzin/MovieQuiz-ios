80eae126db774421e783a774c3679d47809536e4
EvgeniyKuzin <vaglover@yandex.ru>
2024-05-26 15:12:54 +0300

    начало перенола алерта

---
+++ MovieQuiz/Models/AlertModel.swift
@@ -0,0 +1,8 @@
+//
+//  AlertModel.swift
+//  MovieQuiz
+//
+//  Created by Evgeniy Kuzin on 26.05.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  AlertPresenter.swift
+//  MovieQuiz
+//
+//  Created by Evgeniy Kuzin on 26.05.2024.
+//
+
+import Foundation
